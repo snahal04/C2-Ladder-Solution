f24aa4ec7b7d51ede21e923bfa18a94a38d6d2b3
Snahal Kumar <77937488+snahal04@users.noreply.github.com>
2023-01-09 20:17:40 +0530

    Create Find_The_Array.cpp

---
+++ 800 Rating/Find_The_Array.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;cin>>n;
+    float a = ceil(sqrt(n));
+    cout<<a<<endl;
+}
+
+
+int main(){
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
+}
